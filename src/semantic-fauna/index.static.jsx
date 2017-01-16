b1a68473138dd6ff5217601182eb560e89254ce0
Dan Coates <dan.coates@blueflag.com.au>
2017-01-16 12:32:48 +1100

    Updating file paths

--- src/semantic-fauna/index.static.jsx
+++ src/semantic-fauna/index.static.jsx
@@ -15,7 +15,7 @@ export default function index(props: IndexProps): React.Element<any> {
             <title>semantic-fauna</title>
             <meta name="description" content=""/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            {props.css && props.css.map(file => <link key={file} rel="stylesheet" href={file}/>)}
+            {props.css && props.css.map(file => <link key={file} rel="stylesheet" href={file.substring(1)}/>)}
         </head>
         <body>
             <div id="semantic-fauna" dangerouslySetInnerHTML={{__html: props.content}}></div>
@@ -25,7 +25,7 @@ export default function index(props: IndexProps): React.Element<any> {
                 So the below line is required to avoid errors when the bundle sets module.exports
             */}
             <script dangerouslySetInnerHTML={{__html: 'window.module = {};'}}/>
-            {props.scripts && props.scripts.map(file => <script key={file} src={file}/>)}
+            {props.scripts && props.scripts.map(file => <script key={file} src={file.substring(1)}/>)}
 
         </body>
     </html>;
